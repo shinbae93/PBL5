b318eb6dcd896a32ba052cf2163a7ba82c73ad12
leminhtridnvn01 <leminhtridnvn01@gmail.com>
2022-06-01 23:11:05 +0700

    album

--- code/albums/views.py
+++ code/albums/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from artists.models import Artist
 
+# Create your views here.
+def show(request):
+    firstname = request.session["firstname"]
+    artists = Artist.objects.all()
+    return render(request, 'authentication/album.html', {"artists": artists,"firstname": firstname})
 # Create your views here.
